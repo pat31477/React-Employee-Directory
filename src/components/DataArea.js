48578dcd159e24bed1f72f7b410cfb048c3df31f
Pat31477 <pat31477@yahoo.com>
2021-06-21 18:41:11 -0400

    fixed typos in DataArea.js file

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import DataTable from "./DataTable";
 import Nav from "./Nav";
 import API from "../utils/API";
-//import "../styles/DataArea.css";
 
 export default class DataArea extends Component {
     state = {
@@ -31,26 +30,26 @@ export default class DataArea extends Component {
 
         const compareFnc = (a, b) => {
             if (this.state.order === "ascend") {
-                //account for missing values
+                
                 if (a[heading] === undefined) {
                     return 1;
                 } else if (b[heading] === undefined) {
                     return -1;
                 }
-                //numerically
+                
                 else if (heading === "name") {
                     return a[heading].first.localeCompare(b[heading].first);
                 } else {
                     return a[heading] - b[heading];
                 }
             } else {
-                //(account for missing values
+                
                 if (a[heading] === undefined) {
                     return 1;
                 } else if (b[heading] === undefined) {
                     return -1;
                 }    
-            //numerically
+            
                 else if (heading === "name") {
                     return b[heading].first.localeCompare(a[heading].first);
                 } else {
@@ -66,7 +65,7 @@ export default class DataArea extends Component {
         console.log(event.target.value);
         const filter = event.target.value;
         const filteredList = this.state.users.filter(item => {
-            //merge data together, then see if user input is antwhere inside
+            
             let values = Object.values(item)
             .join("")
             .toLowerCase();
