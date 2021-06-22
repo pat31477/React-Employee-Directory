fedbdc9e6821ad3a17d95844a131773feec39405
Pat31477 <pat31477@yahoo.com>
2021-06-22 19:36:40 -0400

    changed headings percentages

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -11,11 +11,11 @@ export default class DataArea extends Component {
     }
 
     headings = [
-        { name: "Image", width: "10%" },
-        { name: "Name", width: "10%" },
-        { name: "Phone", width: "20%" },
-        { name: "Email", width: "20%" },
-        { name: "DOB", width: "10%" }
+        { name: "Image", width: "8%" },
+        { name: "Name", width: "15%" },
+        { name: "Phone", width: "25%" },
+        { name: "Email", width: "25%" },
+        { name: "DOB", width: "15%" }
     ]
 
     handleSort = heading => {
